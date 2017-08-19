350773f852f33475ea35408054e6cc1265ad170b
matthew-chun <31026513+matthew-chun@users.noreply.github.com>
2017-08-18 23:47:08 -0400

    Did the lunge?
    Created the weapon sprite, linked it to the player so that the movement is the same. Did the lunge, its pretty lame atm tho, still gotta think some things over about it.

---
+++ Project_Fence/Assets/Scripts/Lunge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lunge : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public Vector3 move;
+	public Vector3 recov;
+
+	void Update () {
+		if(Input.GetKeyDown("z"))
+        {
+            move = new Vector3(1, 0, 0);
+            transform.position += move * speed * Time.deltaTime;
+            recov = new Vector3(-1, 0, 0);
+            transform.position += move * speed * Time.deltaTime;
+        }
+	}
+}
